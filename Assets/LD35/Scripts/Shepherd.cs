c195e4519fe3459a8380e59d8cad3a10357e543c
almyu <hire@indeed.li>
2016-04-16 12:17:50 +0700

    Add cubic shepherd

--- Assets/LD35/Scripts/Shepherd.cs
+++ Assets/LD35/Scripts/Shepherd.cs
@@ -4,5 +4,22 @@ using System.Collections;
 namespace LD35 {
 
     public class Shepherd : MonoBehaviour {
+
+        public float speed = 4f;
+
+        private void Update() {
+            var axes = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+            axes.Normalize();
+
+            if (axes != Vector3.zero) {
+                var camXf = Camera.main.transform;
+                var dir =
+                    camXf.right.WithY(0f).normalized * axes.x +
+                    camXf.forward.WithY(0f).normalized * axes.z;
+
+                transform.position += dir * Time.deltaTime;
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 7f);
+            }
+        }
     }
 }
