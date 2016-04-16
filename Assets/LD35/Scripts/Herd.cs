11bc4463e0c9839744d021e11bb06dddbc99b8ce
almyu <hire@indeed.li>
2016-04-16 12:24:46 +0700

    Add herd

---
+++ Assets/LD35/Scripts/Herd.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace LD35 {
+
+    public class Herd : MonoBehaviour {
+
+        public int numSheep = 10;
+        public float spawnRadius = 5f;
+        public Sheep sheepPrefab;
+
+        private void Awake() {
+            for (int i = 0; i < numSheep; ++i)
+                SpawnSheep();
+        }
+
+        public void SpawnSheep() {
+            var pos = transform.position + Random.insideUnitSphere.WithY(0f) * spawnRadius;
+            Instantiate(sheepPrefab, pos, Quaternion.AngleAxis(Random.value * 360f, Vector3.up));
+        }
+    }
+}
