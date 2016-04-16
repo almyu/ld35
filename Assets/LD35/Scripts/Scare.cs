8fd93ab88de1c41f2de07ffe8daf34d79971127e
almyu <hire@indeed.li>
2016-04-16 15:11:00 +0700

    Let sheep bounce

--- Assets/LD35/Scripts/Scare.cs
+++ Assets/LD35/Scripts/Scare.cs
@@ -19,22 +19,28 @@ namespace LD35 {
             list.RemoveAt(list.Count - 1);
         }
 
-        public static Vector3 GetEscapeVector(Vector3 position) {
-            var escape = Vector3.zero;
+        public static void GetEscapeVector(Vector3 position, out Vector3 escape, out Vector3 bounce) {
+            escape = Vector3.zero;
+            bounce = Vector3.zero;
 
             foreach (var scare in list) {
                 var dir = position - scare.transform.position;
                 var distSq = dir.sqrMagnitude;
-                escape += (1f - Mathf.Clamp01(distSq / scare.sqrRadius)) * scare.power * dir.normalized;
+                if (Mathf.Approximately(distSq, 0f)) continue;
+
+                dir /= Mathf.Sqrt(distSq);
+                escape += (1f - Mathf.Clamp01(distSq / scare.sqrRadius)) * scare.power * dir;
+
+                if (distSq < scare.sqrBounceRadius)
+                    bounce += scare.bouncePower * dir;
             }
-            return escape;
         }
 
         public float radius = 2f, power = 1f;
+        public float sqrRadius { get { return radius * radius; } }
 
-        public float sqrRadius {
-            get { return radius * radius; }
-        }
+        public float bounceRadius = 1f, bouncePower = 10f;
+        public float sqrBounceRadius { get { return bounceRadius * bounceRadius; } }
 
         private void OnEnable() {
             Register(this);
--- Assets/LD35/Scripts/Sheep.cs
+++ Assets/LD35/Scripts/Sheep.cs
@@ -6,18 +6,32 @@ namespace LD35 {
     public class Sheep : Scare {
 
         public float speed = 5f;
+        public float bounceInertia = 0.95f;
+
+        private Vector3 bounce;
 
         private void FixedUpdate() {
-            var vel = Scare.GetEscapeVector(transform.position);
-            transform.position += speed * Time.fixedDeltaTime * vel.WithY(0f);
+            Vector3 vel;
+            Scare.GetEscapeVector(transform.position, out vel, out bounce);
 
-            if (vel != Vector3.zero)
+            if (vel != Vector3.zero) {
+                transform.position += speed * Time.fixedDeltaTime * vel.WithY(0f);
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(vel), Time.deltaTime * 7f);
+            }
+
+            transform.position += Time.deltaTime * bounce.WithY(0f);
+            bounce *= bounceInertia;
         }
 
         private void OnDrawGizmos() {
+            Vector3 vel, bnc;
+            Scare.GetEscapeVector(transform.position, out vel, out bnc);
+
             Gizmos.color = Color.red;
-            Gizmos.DrawRay(transform.position, Scare.GetEscapeVector(transform.position));
+            Gizmos.DrawRay(transform.position, vel);
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(transform.position, bnc);
         }
     }
 }
