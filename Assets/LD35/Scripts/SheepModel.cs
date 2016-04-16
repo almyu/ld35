1cf9d3a14d7e4a30d4b17d9976e089dbf987a186
almyu <hire@indeed.li>
2016-04-16 14:38:36 +0700

    Generate wool

---
+++ Assets/LD35/Scripts/SheepModel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace LD35 {
+
+    public class SheepModel : MonoBehaviour {
+
+        public MeshFilter body;
+        public Mesh ballMesh;
+        public Vector2 ballScaleRange = new Vector2(0.9f, 1.1f);
+
+        private void Awake() {
+            var vertices = body.sharedMesh.vertices;
+            var normals = body.sharedMesh.normals;
+
+            var submeshes = new CombineInstance[vertices.Length];
+
+            for (int i = 0; i < submeshes.Length; ++i) {
+                submeshes[i] = new CombineInstance {
+                    mesh = ballMesh,
+                    transform = Matrix4x4.TRS(
+                        vertices[i],
+                        Quaternion.LookRotation(normals[i]),
+                        Random.Range(ballScaleRange.x, ballScaleRange.y) * Vector3.one)
+                };
+            }
+
+            var combinedMesh = new Mesh();
+            combinedMesh.CombineMeshes(submeshes, true, true);
+            body.sharedMesh = combinedMesh;
+        }
+    }
+}
