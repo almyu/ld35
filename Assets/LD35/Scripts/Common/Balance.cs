c321eb2b7432ab8ff48fb21a885fa0a20162c441
sxwx <DeadGeorge>
2016-04-16 11:58:28 +0700

    Add dog and balance scripts

---
+++ Assets/LD35/Scripts/Common/Balance.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LD35
+{
+    public class Balance : MonoSingleton<Balance>
+    {
+        [Header("Dogs properties")]
+        public float DogMoveSpeed = 1f;
+    }
+}
--- Assets/LD35/Scripts/Dog.cs
+++ Assets/LD35/Scripts/Dog.cs
@@ -1,5 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class Dog : MonoBehaviour {
+namespace LD35
+{
+    public class Dog : MonoBehaviour
+    {
+        private Camera _camera;
+        private int leftMouseBtn = 0;
+
+        protected void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        protected void Update()
+        {
+            if (Input.GetMouseButtonUp(leftMouseBtn))
+            {
+                var target = _camera.ScreenToWorldPoint(Input.mousePosition);
+                StopCoroutine("Run");
+                StartCoroutine(Run(target));
+            }
+        }
+
+        private IEnumerator Run(Vector3 target, Action onComplete = null)
+        {
+            var start = transform.position;
+            var runSpeed = Balance.instance.DogMoveSpeed;
+            var elapsed = 0f;
+
+            while (Vector3.Distance(start, target) > 0)
+            {
+                elapsed += Time.deltaTime;
+                transform.position = Vector3.Lerp(start, target, elapsed / runSpeed);
+                yield return new WaitForEndOfFrame();
+            }
+
+            transform.position = target;
+
+            if (onComplete != null)
+                onComplete();
+        }
+    }
 }
