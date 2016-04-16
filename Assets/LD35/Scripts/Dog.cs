58b54a616f627157748aeb117583e0e8560b2b5f
sxwx <DeadGeorge>
2016-04-16 13:08:00 +0700

    Tweak dog move

--- Assets/LD35/Scripts/Dog.cs
+++ Assets/LD35/Scripts/Dog.cs
@@ -9,10 +9,12 @@ namespace LD35
         private Camera _camera;
         private int leftMouseBtn = 0;
         private int rightMouseBtn = 1;
+        private LayerMask island;
 
         protected void Awake()
         {
             _camera = Camera.main;
+            island = LayerMask.GetMask("Island");
         }
 
         protected void Update()
@@ -22,7 +24,7 @@ namespace LD35
                 var ray = _camera.ScreenPointToRay(Input.mousePosition);
 
                 RaycastHit hit;
-                if (Physics.Raycast(ray, out hit))
+                if (Physics.Raycast(ray, out hit, float.PositiveInfinity, island))
                 {
                     StopCoroutine("Run");
                     StartCoroutine(Run(hit.point));
@@ -35,6 +37,7 @@ namespace LD35
         {
             var start = transform.position;
             var runSpeed = Balance.instance.DogMoveSpeed;
+            var runCurve = Balance.instance.DogMoveCurve;
             var elapsed = 0f;
 
             var distance = Vector3.Distance(start, target);
@@ -43,7 +46,7 @@ namespace LD35
             while (Vector3.Distance(start, target) > 0)
             {
                 elapsed += Time.deltaTime;
-                transform.position = Vector3.Lerp(start, target, elapsed / currentSpeed);
+                transform.position = Vector3.Slerp(start, target, runCurve.Evaluate(elapsed / currentSpeed));
                 yield return new WaitForEndOfFrame();
             }
 
