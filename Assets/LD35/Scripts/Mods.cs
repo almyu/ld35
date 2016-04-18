be93d289304e771b0d89a53168172f5c5d7c0108
almyu <hire@indeed.li>
2016-04-18 11:20:01 +0700

    Update mods

--- Assets/LD35/Scripts/Mods.cs
+++ Assets/LD35/Scripts/Mods.cs
@@ -7,17 +7,42 @@ namespace LD35 {
         public string name, desc;
     }
 
+    public class ModID {
+        public const int
+            None = -1,
+            EatAll = 0,
+            LoseAll = 1,
+            RedSheep = 2,
+            BlackSheep = 3,
+            YellowSheep = 4,
+            Wind = 5,
+            Faster = 6,
+            Scarier = 7,
+            Diet = 8,
+            UndertrainedDog = 9,
+            MaxID = 10;
+    }
+
 
     public class Mods {
 
         public static int numMods {
-            get { return modList.Length; }
+            get { return ModID.MaxID; }
         }
 
-        public static readonly Mod[] modList = new[] {
-            new Mod { name = "Test 0", desc = "Do stuff" },
-            new Mod { name = "Test 1", desc = "Do moar stuff" },
-            new Mod { name = "Test 2", desc = "Do some crazy shit" }
-        };
+        public static readonly Mod[] modList = new Mod[ModID.MaxID];
+
+        static Mods() {
+            modList[ModID.EatAll] = new Mod { name = "Eat All" };
+            modList[ModID.LoseAll] = new Mod { name = "Lose All" };
+            modList[ModID.RedSheep] = new Mod { name = "Red Sheep", desc = "Eat 10 red sheep" };
+            modList[ModID.BlackSheep] = new Mod { name = "Black Sheep", desc = "Eat the black sheep last" };
+            modList[ModID.YellowSheep] = new Mod { name = "Yellow Sheep", desc = "Eat the yellow sheep" };
+            modList[ModID.Wind] = new Mod { name = "Wind", desc = "Eat 20 sheep" };
+            modList[ModID.Faster] = new Mod { name = "Faster!", desc = "Eat 20 sheep" };
+            modList[ModID.Scarier] = new Mod { name = "Scarier!", desc = "Eat 20 sheep" };
+            modList[ModID.Diet] = new Mod { name = "Diet", desc = "Eat 20 sheep" };
+            modList[ModID.UndertrainedDog] = new Mod { name = "Undertrained Dog", desc = "Eat 15 sheep" };
+        }
     }
 }
