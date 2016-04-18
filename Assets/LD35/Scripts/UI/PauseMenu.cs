d556ae6d6eaac5fc2b61899e10e2222eca5d836e
almyu <hire@indeed.li>
2016-04-19 05:14:18 +0700

    Update pause logic

--- Assets/LD35/Scripts/UI/PauseMenu.cs
+++ Assets/LD35/Scripts/UI/PauseMenu.cs
@@ -1,13 +1,52 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 namespace LD35 {
 
     public class PauseMenu : MonoSingleton<PauseMenu> {
 
-        public static void Toggle() {
-            if (instance) SceneManager.UnloadScene("PauseMenu");
-            else SceneManager.LoadScene("PauseMenu", LoadSceneMode.Additive);
+        public enum Mode {
+            Pause,
+            Death,
+            GameOver
+        }
+        public static Mode mode;
+
+        private static bool Load() {
+            if (instance) return false;
+
+            SceneManager.LoadScene("PauseMenu", LoadSceneMode.Additive);
+            return true;
+        }
+
+        public static void Pause() {
+            mode = Mode.Pause;
+            if (!Load()) SceneManager.UnloadScene("PauseMenu");
+        }
+
+        public static void Die() {
+            mode = Mode.Death;
+            Load();
+        }
+
+        public static void GameOver() {
+            mode = Mode.GameOver;
+            Load();
+        }
+
+        public Text labelPaused, labelDied, labelGameOver;
+
+        private void OnEnable() {
+            Time.timeScale = 0f;
+
+            if (labelPaused) labelPaused.gameObject.SetActive(mode == Mode.Pause);
+            if (labelDied) labelDied.gameObject.SetActive(mode == Mode.Death);
+            if (labelGameOver) labelGameOver.gameObject.SetActive(mode == Mode.GameOver);
+        }
+
+        private void OnDisable() {
+            Time.timeScale = 1f;
         }
     }
 }
