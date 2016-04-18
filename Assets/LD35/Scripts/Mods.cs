f4d20bea7bc67927445c80e034fa2059a816e330
almyu <hire@indeed.li>
2016-04-18 10:13:46 +0700

    Add mods stub

---
+++ Assets/LD35/Scripts/Mods.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace LD35 {
+
+    public class Mod {
+        public bool active;
+        public string name, desc;
+    }
+
+
+    public class Mods {
+
+        public static int numMods {
+            get { return modList.Length; }
+        }
+
+        public static readonly Mod[] modList = new[] {
+            new Mod { name = "Test 0", desc = "Do stuff" },
+            new Mod { name = "Test 1", desc = "Do moar stuff" },
+            new Mod { name = "Test 2", desc = "Do some crazy shit" }
+        };
+    }
+}
