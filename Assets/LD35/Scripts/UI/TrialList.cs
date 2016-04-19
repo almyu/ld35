75e6e9012a8112467c421f5f5b9cbfd870e81a19
almyu <hire@indeed.li>
2016-04-19 07:03:32 +0700

    Tweak trial colors

--- Assets/LD35/Scripts/UI/TrialList.cs
+++ Assets/LD35/Scripts/UI/TrialList.cs
@@ -6,6 +6,11 @@ namespace LD35 {
 
     public class TrialList : HierarchyBuilder<ModTrial> {
 
+        public Color
+            activeColor = new Color32(137, 30, 46, 255),
+            completedColor = Color.white,
+            inactiveColor = Color.grey;
+
         protected override void Build() {
             if (!GameRun.instance) return;
 
@@ -14,14 +19,28 @@ namespace LD35 {
         }
 
         protected override void Tune(Transform spawn, ModTrial data) {
-            foreach (var img in spawn.GetComponentsInChildren<Image>(true)) {
-                if (img.name.StartsWith("Checkbox")) {
+            foreach (var img in spawn.GetComponentsInChildren<Image>(true))
+                if (img.name.StartsWith("Checkbox"))
                     img.gameObject.SetActive(img.name == "Checkbox" + data.status);
-                }
-            }
+
             foreach (var text in spawn.GetComponentsInChildren<Text>()) {
                 if (text.name == "Text") text.text = data.name;
                 else if (text.name == "Counter") text.text = string.Format("{0}/{1}", data.current, data.max);
+
+                switch (data.status) {
+                case ModStatus.Active:
+                case ModStatus.Failed:
+                    text.color = activeColor;
+                    break;
+
+                case ModStatus.Completed:
+                    text.color = completedColor;
+                    break;
+
+                default:
+                    text.color = inactiveColor;
+                    break;
+                }
             }
         }
 
